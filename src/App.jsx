294c37be13511a19815620d692b3099a974ba70f
José Bernal <jbernal.web.dev@gmail.com>
2024-10-16 20:18:31 -0700

    Update git subodules.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /** @jsx vNode */ /** @jsxFrag "Fragment" */
 import { vNode, View } from "@ocdla/view";
-import Header from "./components/Header";
+// import Header from "./components/Header";
+import Navigation from "@ocdla/global-components/src/navigation/Navigation";
 import Footer from "./components/Footer";
 import routes from "./data/routes.js";
 import Router from "@ocdla/lib-routing/src/Router.js";
@@ -31,11 +32,42 @@ console.log(Page, HeaderTwo);
 let location = router.getLocation();
 
 export default function App() {
-
+    let items = [
+        {
+            url: "/",
+            label: "home"
+        },
+        {
+            url: "/events",
+            label: "events"
+        },
+        {
+            url: "/eat",
+            label: "food",
+            hidden: true
+        },
+        {
+            url: "/drink",
+            label: "drink",
+            hidden: false
+        },
+        {
+            url: "/about",
+            label: "about",
+            hidden: true
+        },
+        {
+            url: "/contact",
+            label: "contact us",
+            hidden: true
+        }
+    ];
 
     return (
         <>
-            <Header />
+            <header class="w-full mb-0 pb-1 p-[10px] sticky top-0 bg-wb-black z-50">
+                <Navigation routes={items} />
+            </header>
             {typeof HeaderTwo === "function" ? <HeaderTwo /> : <></>}
             <main class={`grow overflow-x-hidden font-default-paragraph ${location}`}>
                 <Page />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 /** @jsx vNode */ /** @jsxFrag "Fragment" */
 import { vNode, View } from "@ocdla/view";
 import Navigation from "./navigation/Navigation";
+
+
+
 export default function Header() {
     return (
         <header class="w-full mb-0 pb-1 p-[10px] sticky top-0 bg-wb-black z-50">
+            <img src="/src/images/hand-on-mug.png" alt="Wandering Bard logo" class="w-12 h-12" />
             <Navigation />
         </header>
     );
